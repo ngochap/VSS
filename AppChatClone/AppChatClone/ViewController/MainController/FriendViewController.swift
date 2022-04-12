29f2230e0233d9ace97770d69a083484c5079d1f
NgocHap <ngochap98@gmail.com>
2022-04-12 23:09:19 +0700

    orign

--- AppChatClone/AppChatClone/ViewController/MainController/FriendViewController.swift
+++ AppChatClone/AppChatClone/ViewController/MainController/FriendViewController.swift
@@ -12,21 +12,54 @@ class FriendViewController: UIViewController {
     @IBOutlet weak var viewBottom: UIView!
     @IBOutlet weak var viewMenu: UIView!
     
-    let bottomView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
+    var hori : NSLayoutConstraint?
     override func viewDidLoad() {
         super.viewDidLoad()
         viewBottom.roundCorners(corners: [.topLeft, .topRight], radius: 40)
         
         self.viewMenu.layer.borderWidth = 1
         self.viewMenu.layer.borderColor = hexStringToUIColor(hex: "999999").cgColor
+        setupHorizonalBar()
+    }
+    func setupHorizonalBar(){
         
+        let horizonal = UIView()
+        horizonal.backgroundColor = hexStringToUIColor(hex: "5B71E2")
+        horizonal.translatesAutoresizingMaskIntoConstraints = false
+        viewMenu.addSubview(horizonal)
+        
+        horizonal.bottomAnchor.constraint(equalTo: viewMenu.bottomAnchor).isActive = true
+        horizonal.widthAnchor.constraint(equalTo: viewMenu.widthAnchor, multiplier: 1/3).isActive = true
+        horizonal.heightAnchor.constraint(equalToConstant: 4).isActive = true
+        hori = horizonal.leftAnchor.constraint(equalTo: self.viewMenu.leftAnchor, constant: 10)
+      
+        hori?.isActive = true
     }
-    private func setupLayout() {
-        view.addSubview(bottomView)
-        bottomView.topAnchor.constraint(equalTo: viewBottom.bottomAnchor).isActive = true
+//    func scrollMenuItemIndex(menuIndex: Int){
+//        let indexPath = NSIndexPath(item: menuIndex, section: 0)
+//        playCollection.scrollToItem(at: indexPath as IndexPath, at: [], animated: true)
+//    }
+    @IBAction func clickList(_ sender: UIButton) {
+        let x = CGFloat(sender.tag) * viewMenu.frame.width / 3 + 10
+        hori?.constant = x
+//        scrollMenuItemIndex(menuIndex: sender.tag)
+        UIView.animate(withDuration: 0.4, animations: {
+            self.viewMenu.layoutIfNeeded()
+        })
+//        switch sender.tag{
+//            case 0:
+//                defaultbtn()
+//            case 1:
+//                lbBass.textColor = hexStringToUIColor(hex: "67AC47")
+//                lbEqualizer.textColor = .white
+//                lbExtra.textColor = .white
+//            case 2:
+//                lbExtra.textColor = hexStringToUIColor(hex: "67AC47")
+//                lbBass.textColor = .white
+//                lbEqualizer.textColor = .white
+//            default: break
+//        }
+
     }
+
 }
